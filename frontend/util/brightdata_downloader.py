9f440aee1a3574102a07530cb4926b678dcd2419
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-03-20 22:48:14 +0500

    improve ui and add better vector

--- frontend/util/brightdata_downloader.py
+++ frontend/util/brightdata_downloader.py
@@ -3,3 +3,14 @@ import time
 from typing import Dict, Optional
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+class BrightDataDownloader:
+    def __init__(self):
+        self.base_url = "https://api.brightdata.com"
+        self.auth_token = os.getenv('BRIGHTDATA_API_KEY')
+        self.headers = {
+            "Authorization": f"Bearer {self.auth_token}",
+            "Content-Type": "application/json"
+        }
